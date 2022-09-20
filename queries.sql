6955b2df9621e50d1a92e5f9e101d1cda757ce87
panda1 <blueblue0011@gmail.com>
2022-09-19 17:43:40 -0700

    added 2 insert

---
+++ queries.sql
@@ -0,0 +1,20 @@
+insert into customer (username, password, created_at)
+values
+('wilderness', 'password1', '2022-2-2'),
+('brandytea', 'password2', '2022-2-3'),
+('theminions', 'password3', '2022-2-4'),
+('thematrix', 'password4', '2022-2-5'),
+('taxidriver', 'password5', '2022-2-6');
+
+insert into item (name, price)
+values
+('Apple pie', '20.00'),
+('avocado', '5.00'),
+('moggle mini-pancake', '7.00'),
+('squid ink pasta', '19.60'),
+('chocobo curry', '17.00'),
+('titan, god of the crags weight of the land burger', '17.60'),
+('x-aether', '14.30'),
+('mog tropical pineapple defeat battle', '24.60'),
+('claim differdange', '13.00'),
+('potion', '12.00');
